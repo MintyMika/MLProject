5e3c9724e1295fb704380fba427278cb0063632d
Stephen C <83474041+MintyMika@users.noreply.github.com>
2024-04-21 16:26:31 -0500

    Stop being orange

--- getNumSongs.py
+++ getNumSongs.py
@@ -13,17 +13,34 @@ with alive_bar(num_files) as bar:
         with open(os.path.join(dataset_path, file), "r") as f:
             data = json.load(f)
             for playlist in data["playlists"]:
+                list_of_artists = []
                 for song in playlist["tracks"]:
                     # if the URI is not in the set then add it with value 1
-                    if song["artist_uri"] not in dictOfURIs:
-                        # TODO: Fix this
-                        dictOfURIs.update({song["artist_uri"]: 1})
+                    if song["artist_uri"] not in dictOfURIs and song["artist_uri"] not in list_of_artists:
+                        dictOfURIs.update({song["artist_uri"]: [1, song["artist_name"]]})
+                        list_of_artists.append(song["artist_uri"])
+                        
+                    if song["artist_uri"] in list_of_artists:
+                        continue
+
                     else:
-                        dictOfURIs[song["artist_uri"]] += 1
-                    # if it is in the set then increment the value by 1
+                        dictOfURIs[song["artist_uri"]][0] += 1
+                        list_of_artists.append(song["artist_uri"])
             bar()
 
-# Only count the songs that appear more than 10 times
-newDictOfURIs = {key: value for key, value in dictOfURIs.items() if value > 50}
 
-print(f"Number of songs: {len(newDictOfURIs):,}")
+# open a file to write the dictionary to
+file_name = r"Dataset\dictOfURIs.json"
+
+# Only count the songs that appear more than 50 times
+newDictOfURIs = {}
+dictofArtists = {}
+for key, value in dictOfURIs.items():
+    if value[0] > 50:
+        newDictOfURIs.update({key: value})
+        dictofArtists.update({key: value[1]})
+
+with open(file_name, "w") as f:
+    json.dump(dictofArtists, f)
+
+print(f"Number of Artists: {len(newDictOfURIs):,}")
