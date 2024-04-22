2712176431b4579c5926bbe74c784480aa8efd34
Stephen C <83474041+MintyMika@users.noreply.github.com>
2024-04-21 19:56:09 -0500

    stupid code

--- formatDataset.py
+++ formatDataset.py
@@ -4,7 +4,7 @@ import pandas as pd
 from alive_progress import alive_bar
 import threading
 
-dictOfArtists = json.load(open(r"src\dictOfArtists.json", "r"))
+dictOfArtists = json.load(open(r"Dataset\src\dictOfURIs.json", "r"))
 
 # Make a new dictionary with artist_URI as key and a number as value
 numberedArtists = {}
@@ -20,8 +20,12 @@ num_files = len([name for name in os.listdir(dataset_path)])
 
 iters = 0
 
-with alive_bar(num_files) as bar:
+with alive_bar(int(num_files/4)) as bar:
     for file in os.listdir(dataset_path):
+        # Only go through 25% of the files
+        iters += 1
+        if iters == 250:
+            break
         with open(os.path.join(dataset_path, file), "r") as f:
             data = json.load(f)
             for playlist in data["playlists"]:
@@ -36,27 +40,8 @@ with alive_bar(num_files) as bar:
     
 
 # Save the dictionary to a file
-with open(r"src\AllPlaylists.json", "w") as f:
+with open(r"Dataset\src\dictOfPlaylists.json", "w") as f:
     json.dump(dictOfPlaylists, f)
 
 # Print the number of playlists
 print(f"Number of playlists: {len(dictOfPlaylists):,}")
-
-series = pd.Series(dictOfPlaylists)
-
-# Split the data into training and testing data
-with alive_bar(1) as bar:
-    training_data, testing_data = train_test_split(series, test_size=0.2)
-    bar()
-
-# Convert the series back to a dictionary
-training_data = training_data.to_dict()
-testing_data = testing_data.to_dict()
-
-with open(r"src\train\TrainingData.json", "w") as f:
-    json.dump(training_data, f)
-
-with open(r"src\test\TestingData.json", "w") as f:
-    json.dump(testing_data, f)
-
-
